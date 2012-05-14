7d080ecd16738941942a6ca82966702eeafca9e3
Venkat Peri <venkatperi@gmail.com>
2012-05-14 17:08:54 -0400

    initial

---
+++ log4cxx/test/test.cpp
@@ -0,0 +1,26 @@
+// test.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <log4cxx/helpers/pool.h>
+#include <log4cxx/fileappender.h>
+#include <log4cxx/patternlayout.h>
+#include "log4cxx/basicconfigurator.h"
+#include <log4cxx/propertyconfigurator.h>
+
+using namespace log4cxx;
+using namespace log4cxx::helpers;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	 //PropertyConfigurator::configure(argv[1]);
+
+	LoggerPtr logger(Logger::getLogger("com.foo"));
+	PatternLayout l();
+
+	LOG4CXX_INFO(logger, "Simple message text.");
+
+	
+	return 0;
+}
+
